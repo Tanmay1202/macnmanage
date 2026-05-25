60d281fcf30cc64e183b0520feebf69def6aa5c1
Tanmay_Singh <singhtanmay1202@gmail.com>
2026-05-25 22:49:58 +0530

    testing the ci/cd pipeline

--- client/src/lib/utils.js
+++ client/src/lib/utils.js
@@ -2,6 +2,6 @@ import { clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs) {
-    // console.log("tanmay");
+    console.log("tanmay");
     return twMerge(clsx(inputs));
 }
